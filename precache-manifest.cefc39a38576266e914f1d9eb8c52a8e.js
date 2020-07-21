3243fc6b34fc4a7c5ad069d447f61fb95d96bfa6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-21 19:19:59 +0000

    Deploy henryz00/COVID-19-Timeline-Data-Visualizer to github.com/henryz00/COVID-19-Timeline-Data-Visualizer.git:gh-pages

---
+++ precache-manifest.cefc39a38576266e914f1d9eb8c52a8e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ad23500271a6d26b795ff7849f12fa",
+    "url": "CNAME"
+  },
+  {
+    "revision": "0ffb457fa2c7afa38a03cc24e6fad2d5",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "c44428c7ad029d74163b94470b939640",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "revision": "47044a4c8a0df144d9c1e59369e71419",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "7817888913c7ef78fd47",
+    "url": "css/app.3623b180.css"
+  },
+  {
+    "revision": "746b1833dc3aaf01c125",
+    "url": "css/chunk-vendors.81c0fe81.css"
+  },
+  {
+    "revision": "dcf101956e855a4b51f2e9ae44ede193",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "2425eba0504630a461ec622156abca6f",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "1b9e87756a32e0211e00ad484b17ad29",
+    "url": "img/logo_grey.1b9e8775.png"
+  },
+  {
+    "revision": "3d0d5353d447f02c10847773a95888bf",
+    "url": "img/qr.3d0d5353.png"
+  },
+  {
+    "revision": "c84ef8946e3327a06f5c589bc0c88093",
+    "url": "index.html"
+  },
+  {
+    "revision": "7817888913c7ef78fd47",
+    "url": "js/app.31741c40.js"
+  },
+  {
+    "revision": "746b1833dc3aaf01c125",
+    "url": "js/chunk-vendors.123e3791.js"
+  },
+  {
+    "revision": "75e2bdb96d4e624c1855055fb3aa22c1",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "37aa45cf56ede32fc566e75ca59800e3",
+    "url": "site.webmanifest"
+  },
+  {
+    "revision": "635c9dd7563c9c85759a34c078e089b6",
+    "url": "style.css"
+  },
+  {
+    "revision": "aa29f115da63f1fcb5342eddce6fd8aa",
+    "url": "thumbnail.png"
+  }
+]);
